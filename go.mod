8d777c05509cc5e1c2fc56000010aaecc8f16165
Brendan Wilde <44901073+Adenosperma1@users.noreply.github.com>
2024-05-21 22:23:03 +1000

    added example files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gopdf.go
 
 go 1.21.6
+
+require github.com/jung-kurt/gofpdf v1.16.2 // indirect
--- gopdf.go
+++ gopdf.go
@@ -2,9 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/jung-kurt/gofpdf"
 )
 
 
 func main() {
-fmt.Println("hello")
+// fmt.Println("hello")
+file := "txtForPdf.txt"
+content, err := os.ReadFile(file)
+if err != nil {
+	log.Fatalf("%s file not found", file)
+} else {
+	fmt.Println("read file")
+}
+
+pdf := gofpdf.New("P", "mm", "A4", "")
+
+pdf.AddPage()
+pdf.SetFont("Arial", "B", 14)
+pdf.MultiCell(190.0, 277.0, string(content), "", "", false)
+
+err = pdf.OutputFileAndClose("test.pdf")
+
+if err != nil {
+	log.Fatal("failed to make pdf")
+} else {
+	fmt.Println("Created text.pdf")
+}
+
 }
